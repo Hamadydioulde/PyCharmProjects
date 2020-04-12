9e30ae88054d7193a26dd2a6ac3e6267ad986f5e
hamady <hamadyka92@gmail.com>
2020-04-12 07:45:27 +0200

    Commit done through PyCharm

--- one.py
+++ one.py
@@ -1,16 +1,14 @@
 #print("hello world hamady welcome\n")
 #print("Finally!!!!!\n")
-
+#it-s me again VCS through pycharm
 def ham(msg):
     print(msg)
 
 ham("ham function that prints the message msg")
 
 #Thanks See you soon Guys Peace!!!!!!!!!!!!!!!!
-<<<<<<< HEAD
 
 
 #adding something second commit
-=======
 #Heyyy!!!
->>>>>>> refs/remotes/origin/master
+#>>>>>>> refs/remotes/origin/master
